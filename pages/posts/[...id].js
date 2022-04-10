b0ebc0c6ae112c8ab16f3f78095420d5a8f31991
fqqk <fqqk.work68@gmail.com>
2022-04-10 17:24:21 +0900

    catch-all-route検証

--- pages/posts/[...id].js
+++ pages/posts/[...id].js
@@ -31,7 +31,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+  const postData = await getPostData(params.id.join("/"));
   return {
     props: {
       postData,
