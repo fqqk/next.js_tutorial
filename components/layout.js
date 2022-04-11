06a635cf9d58f54dbfd5a08e472e0be69a1a1fe1
fqqk <fqqk.work68@gmail.com>
2022-04-11 09:38:37 +0900

    Headコメントアウト

--- components/layout.js
+++ components/layout.js
@@ -11,8 +11,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        {home && (
-          <>
+        {/*           
             <link rel="icon" href="/favicon.ico" />
             <meta
               name="description"
@@ -25,9 +24,7 @@ export default function Layout({ children, home }) {
               )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
             />
             <meta name="og:title" content={siteTitle} />
-            <meta name="twitter:card" content="summary_large_image" />
-          </>
-        )}
+            <meta name="twitter:card" content="summary_large_image" /> */}
       </Head>
       <header className={styles.header}>
         {home ? (
